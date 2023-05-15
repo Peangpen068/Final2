dabb02787a1d93776826d34a63215cd42f14a0e8
Peangpen068 <123537577+Peangpen068@users.noreply.github.com>
2023-05-15 16:09:04 +0700

    Add files via upload

---
+++ Programfinal2.1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the maximum tank capacity (Vmax):");
+        double Vmax = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter the average volume of water consumed during breaks (VdrinkVmax):");
+        double VdrinkVmax = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter the time duration between breaks (tdaytdrink) in hours:");
+        int tdaytdrink = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the time duration between refills (tdaytfill) in hours:");
+        int tdaytfill = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the time duration of the entire activity (ttotal) in hours:");
+        int ttotal = Convert.ToInt32(Console.ReadLine());
+
+        double Vdrink = VdrinkVmax * Vmax;
+        double Vfill = Vmax - Vdrink;
+        double Vremaining = Vmax;
+        int breaks = ttotal / tdaytdrink;
+        int refills = ttotal / tdaytfill;
+
+        for (int i = 0; i < breaks; i++)
+        {
+            if (Vremaining >= Vdrink)
+            {
+                Vremaining -= Vdrink;
+            }
+            else
+            {
+                Console.WriteLine("Not Enough Water");
+                return;
+            }
+
+            if (i < refills)
+            {
+                if (Vremaining < Vfill)
+                {
+                    Console.WriteLine("Overflow Water");
+                    return;
+                }
+                Vremaining -= Vfill;
+            }
+        }
+
+        if (Vremaining >= 0)
+        {
+            Console.WriteLine("Enough Water, " + Vremaining + " left");
+        }
+    }
+}
+
